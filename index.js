abb39d5b1b81e8a5a91da2d743a50a99c6d5dc7e
Taufik Hidayat Harahap <hrptaufik42@gmail.com>
2022-06-05 17:31:39 +0700

    Membuat fungsi callback

--- index.js
+++ index.js
@@ -6,25 +6,41 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (arrayNama, callbackSort) => {
+    let hasilArray = [];
+
+    callbackSort(arrayNama);
+    for (let index = 0; index < arrayNama.length; index++) {
+        hasilArray.push(index + 1 + "." + " " + arrayNama[index]);
+    }
+    return hasilArray;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (arrayNama) => {
+    return arrayNama.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (arrayNama) => {
+    return arrayNama.reverse();
+};
+
+console.log(sorter(names, sortAscending));
+console.log(sortAscending(names));
+console.log(sortDescending(names));
 
 // ! JANGAN DIMODIFIKASI
-(function main() {
-  console.log(sorter?.(names, sortAscending)?.join("\n"));
-  console.log(sorter?.(names, sortDescending)?.join("\n"));
-})();
+// (function main() {
+//   console.log(sorter?.(names, sortAscending)?.join("\n"));
+//   console.log(sorter?.(names, sortDescending)?.join("\n"));
+// })();
 
-module.exports = {
-  sorter,
-  sortAscending,
-  sortDescending,
-  names,
-};
+// module.exports = {
+//   sorter,
+//   sortAscending,
+//   sortDescending,
+//   names,
+// };
